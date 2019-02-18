de91686e540cb1ecc7af5986154fc5f71a48f07d
RedWars <47759165+RedWars@users.noreply.github.com>
2019-02-18 21:52:18 +0100

    Add files via upload

---
+++ .gitignore/index.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ('.')
+
+bot.on('ready', function() {
+    bot.user.setGame("Surveille Les Serveurs RedWars");
+    console.log("En ligne !");
+});
+
+bot.login("process.env.TOKEN");
+
+bot.on('message', message => {
+    if (message.content === prefix + "help"){
+        message.channel.sendMessage("Liste des commandes \n -*help"); 
+
+    }
+if (message.content === ""){       /// a faire !! space
+    message.reply("")
+    console.log("");
+}
+});
