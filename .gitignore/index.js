beb9d31868567684a80068b01661dfce6768be96
RedWars <47759165+RedWars@users.noreply.github.com>
2019-02-18 22:04:49 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,18 +4,20 @@ const bot = new Discord.Client();
 var prefix = ('.')
 
 bot.on('ready', function() {
-    bot.user.setGame("Surveille Les Serveurs RedWars");
+    bot.user.setActivity("Surveille Les Serveurs RedWars");
     console.log("En ligne !");
 });
 
-bot.login("process.env.TOKEN");
+bot.login(process.env.TOKEN);
 
 bot.on('message', message => {
     if (message.content === prefix + "help"){
         message.channel.sendMessage("Liste des commandes \n -*help"); 
-
+    
     }
-if (message.content === ""){       /// a faire !! space
+
+
+    if (message.content === ""){       /// a faire !! space
     message.reply("")
     console.log("");
 }
