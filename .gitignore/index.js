104fbead525b3cacc31719cf76cb10dbaa6c05ef
RedWars <47759165+RedWars@users.noreply.github.com>
2019-02-18 22:25:50 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 var prefix = ('.')
 
 bot.on('ready', function() {
-    bot.user.setActivity("Surveiller Les Serveurs RedWars");
+    bot.user.setActivity("Surveille Les Serveurs RedWars");
     console.log("En ligne !");
 });
 
@@ -12,13 +12,26 @@ bot.login(process.env.TOKEN);
 
 bot.on('message', message => {
     if (message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes \n -```sa arrive bientôt```"); 
+        message.channel.sendMessage("Liste des commandes \n -*help"); 
     
     }
 
 
-    if (message.content === prefix + "basewars"){       /// a faire !! space
-    message.reply("actuellement en dev :)")
-    console.log("bwinfo effectué");
+    if (message.content === ""){       /// a faire !! space
+    message.reply("")
+    console.log("");
 }
+
+if (message.content === prefix + "embed"){
+    var embed = new Discord.RichEmbed()
+    .setTitle("EMBED")
+    .setDescription("site RedWars")
+    .addField(".web","Page du site RedWars", true)
+    .addField("Site Web", "Voici le lien du site web RedWars (https://redwars.com/", true)
+    .setColor("0xDC143C")
+    .setFooter("Bonne chance sur le site ! :)")
+    message.channel.sendEmbed(embed);
+}
+
 });
+
